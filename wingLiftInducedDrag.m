730a90d3562a9545999a42c39d8ebaf7fb01357f
Nilanjana K <ncv.mit@gmail.com>
2026-05-11 20:43:02 +0530

    Update wingLiftInducedDrag.m
    Used an incorrect estimation to determine the lift around the midspan. With the help of Prandtl's classical lifting line theory, a sequential approach was utilised. First the circulation distribution was determined, followed by lift distribution which were demonstrated by respective plots, and emphasis is shown on numerous outputs rather than just induced drag

--- wingLiftInducedDrag.m
+++ wingLiftInducedDrag.m
@@ -7,24 +7,56 @@ rho = 1.225 ;
 W = 0.9*9.81;
 CL_total = 0.8;
 
+% Determining the velocity
+v = sqrt(2*W/(rho*S*CL_total));
+
 % Splitting the entire wingspan to discrete points
-n = 100;
+n = 1000;
 y = linspace(-b/2,b/2,n);
 theta = acos(2*abs(y)/b);
 
 % Elliptical lift distribution
-CL_y = (CL_total*AR/(pi*e))*sqrt(1-(2*y/b).^2);
+
+% Determining the elliptical shape
+shape = sqrt(1 - (2*y/b).^2);
+
+% Scaling the circulation
+Gamma0 = (4*W)/(pi*rho*V*b);
+
+% Spanwise circulation distribution
+Gamma_y = Gamma0 * shape;
+
+% Lift per unit span:
+L_prime = rho * V .* Gamma_y;
 
 % Induced Drag Estimation
 CDi = CL_total^2/(pi*AR*e);
-Di = 0.5*rho*(2*W/(rho*S*CL_total))^2*S*CDi;
+Di = 0.5*rho*v^2*S*CDi;
 
-% Plotting
-figure;
-plot(y, CL_y, 'b-', 'LineWidth', 3);
-xlabel('Spanwise position y (m)')
-ylabel('Local Lift Coefficient C_{L,y}')
-title('Elliptical Lift Distribution along the Wing')
+% Estimation of downwash
+w = Gamma0/(2*b);
+
+% Plot : Circulation Distribution
+figure
+plot(y, Gamma_y, 'b', 'LineWidth', 2)
+
+xlabel('Spanwise Position y (m)')
+ylabel('Circulation \Gamma(y) (m^2/s)')
+title('Elliptical Circulation Distribution')
 grid on
 
-fprintf('Total Induced Drag = %.3f N\n',Di)
+% Plot: Lift Distribution 
+figure
+plot(y, L_prime, 'r', 'LineWidth', 2)
+
+xlabel('Spanwise Position y (m)')
+ylabel('Lift per Unit Span L''(y) (N/m)')
+title('Spanwise Lift Distribution')
+grid on
+
+% Outputs 
+fprintf('Aspect Ratio              = %.2f\n', AR);
+fprintf('Cruise Velocity           = %.2f m/s\n', V);
+fprintf('Induced Drag Coefficient  = %.4f\n', CDi);
+fprintf('Total Induced Drag        = %.3f N\n', Di);
+fprintf('Estimated Downwash        = %.3f m/s\n', w);
